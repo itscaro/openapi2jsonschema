9f556f09fa50a60f1eaae75b273039eeb084270a
Cyril Jouve <jv.cyril@gmail.com>
2022-10-31 12:40:56 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,4 @@
-#!/usr/bin/env python
-
-from distutils.core import setup
+from setuptools import setup
 
 setup(name='Openapi2jsonschema',
       version='1.0',
@@ -9,6 +7,11 @@ setup(name='Openapi2jsonschema',
       author_email='yann@mandragor.org',
       url='http://github.com/yannh/openapi2jsonschema',
       packages=['openapi2jsonschema'],
+      entry_points={
+        'console_scripts': [
+            'openapi2jsonschema = Openapi2jsonschema.command:default',
+        ]
+      },
       install_requires=[
             'click==7.1.2',
             'jsonref==0.2',
